837fd696a466e8349ae18a9201f1838238ae27f7
kaollie74 <kaollie74@gmail.com>
2019-11-01 14:01:24 -0500

    downloaded jwt package and built the skeleton of authentication for recuring users

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { signup } = require('../controllers/user.controller');
+// IMPORTING MIDDLEWARE
 const { userSignupValidator } = require('../validator/index.validator');
 
+// IMPORTING CONTROLLERS
+const { signup, signin } = require('../controllers/user.controller');
+
+
+// USER ROUTES
 router.post('/signup', userSignupValidator,  signup);
+router.post('/signin', signin)
 
 module.exports = router
