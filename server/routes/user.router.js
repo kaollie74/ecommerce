c5d3eee5de313090037672bc26c9c4c2e27faa49
kaollie74 <kaollie74@gmail.com>
2019-11-01 15:55:47 -0500

    added middleware for authorization and administrator projection

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 // IMPORTING CONTROLLERS
 const { userById } = require('../controllers/user.controller');
-const { requireSignin } = require('../controllers/auth.controller');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.controller');
 
 
 // USER ROUTES
 router.param('userId', userById)// anytime there is a userId in the routes, this will run
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
   res.json({
     user: req.profile
   })
