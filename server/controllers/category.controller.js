c1e0c7352f1e1ac343f4180fdfaab2211ea4d339
kaollie74 <kaollie74@gmail.com>
2019-11-02 11:52:57 -0500

    update

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -6,6 +6,7 @@ const create = (req, res) => {
   const category = new Category(req.body);
 
   category.save((error, data) => {
+    
     if (error) {
       return res.status(400).json({
         error: errorHandler(error)
