8d5d3a48c34fe87efdcf47b5349f4a6bf5dfa9b6
kaollie74 <kaollie74@gmail.com>
2019-11-04 19:31:38 -0600

    able to get single category from db through postman

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,12 +1,28 @@
 const Category = require('../models/category.model');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+const categoryById = (req, res, next, id ) => {
+
+  Category.findById(id).exec((error, category) => {
+
+    if (error || !category) {
+      return res.status(400).json({
+        error: "Category does not exist"
+      })
+    } // END IF
+
+    req.category = category;
+
+    next();
+  })  // END Category.findById
+} // END categoryById
+
 const create = (req, res) => {
 
   const category = new Category(req.body);
 
   category.save((error, data) => {
-    
+
     if (error) {
       return res.status(400).json({
         error: errorHandler(error)
@@ -16,4 +32,11 @@ const create = (req, res) => {
   }) // END category.save()
 } // END create
 
-module.exports = { create } 
+
+const categoryRead = (req, res) => {
+
+  return res.json(req.category);
+
+}
+
+module.exports = { create, categoryById, categoryRead } 
