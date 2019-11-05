7d5dc41ac861122b15f89cb3b56ad498d86da5c9
kaollie74 <kaollie74@gmail.com>
2019-11-04 19:05:22 -0600

    able to update products in the database

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const Product = require('../models/product.model');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+/*********************** PRODUCT BY ID ******************************************* */
 const productById = (req, res, next, id) => {
 
   Product.findById(id).exec((error, product) => {
@@ -20,40 +21,49 @@ const productById = (req, res, next, id) => {
 
 } // END productById.
 
+/*********************** READ ********************************************************/
 const read = (req, res) => {
 
   // set photo property to undefinded which will
-  // prevent being sent back in the request
-  // the retrieving the photo will be handled differently
+  // prevent being sent back in the request.
+  // Retrieving the photo will be handled differently
   // to keep things efficient. 
   req.product.photo = undefined;
   return res.json(req.product);
 
 } // END read
 
+/*********************** CREATE********************************************************/
 const create = (req, res) => {
+
   console.log('In product controller create:', req.product);
+
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
-  form.parse(req, ( error, fields, files) => {
+  form.parse(req, (error, fields, files) => {
 
     // if error return json message which will stop the 
     // rest of the function from running.
     if (error) {
+
       return res.status(400).json({
         error: "Image could not be uploaded"
+
       }) // END res.status
+
     } // END if
 
     // check for all fields 
     // destructuring fields object. 
     const { name, description, price, category, quantity, shipping } = fields;
-    // make sure every field are filled. 
+
+    // make sure every field is filled. 
     if (!name || !description || !price || !category || !quantity || !shipping) {
       return res.status(400).json({
         error: "All fields are required"
       })
-    }
+    } // END IF
+
     // create new product.
     let product = new Product(fields)
 
@@ -64,35 +74,38 @@ const create = (req, res) => {
     // if called 'image' it will be files.image instead. 
     if (files.photo) {
 
-      // check photo size. if over 1mb,
+      // check photo size. if over 1 MB,
       //return an error message. 
       if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: "Image should be less than 1mb in size"
         })
-      } //END IF
+      } // END if (files.photo.size > 1000000)
+
       product.photo.data = fs.readFileSync(files.photo.path)
       //console.log('product.photo.data: ', product.photo.data);
       product.photo.contentType = files.photo.type
       //console.log("product.photo.contentType: ", product.photo.contentType);
 
-    } // END IF(FILES.PHOTO)
+    } // END if(files.photo)
 
-    // save data to new product schema
+    // save data to new product schema,
     // if all conditions are met. 
     product.save((error, result) => {
       if (error) {
         return res.status(400).json({
           error: errorHandler(error)
         })
-      }
+      } // END if(error)
       res.json(result)
-    })
+
+    }) // END product.save()
 
   }) // END form.parse
 
 } // END create
 
+/*********************** REMOVE PRODUCT ************************************************************/
 const removeProduct = (req, res) => {
 
   // get product info from the request
@@ -112,4 +125,79 @@ const removeProduct = (req, res) => {
 
 } // END removeProduct
 
-module.exports = { create, productById, read, removeProduct }; 
+const updateProduct = (req, res) => {
+
+  console.log('In product controller create:', req.product);
+
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (error, fields, files) => {
+
+    // if error return json message which will stop the 
+    // rest of the function from running.
+    if (error) {
+
+      return res.status(400).json({
+        error: "Image could not be uploaded"
+
+      }) // END res.status
+
+    } // END if
+
+    // check for all fields 
+    // grab property from fields object 
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    // make sure every field is filled. 
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: "All fields are required"
+      })
+    } // END IF
+
+    // update req.product, set it to product.
+    let product = req.product;
+    // take two args
+    // existing 'product'
+    // updated 'fields'
+    product = _.extend(product, fields);
+
+
+    // 1kb = 1000;
+    // 1mb = 1,000,000
+    // .photo is how it is sent from the client side
+    // if called 'image' it will be files.image instead. 
+    if (files.photo) {
+
+      // check photo size. if over 1 MB,
+      //return an error message. 
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: "Image should be less than 1mb in size"
+        })
+      } // END if (files.photo.size > 1000000)
+
+      product.photo.data = fs.readFileSync(files.photo.path)
+      //console.log('product.photo.data: ', product.photo.data);
+      product.photo.contentType = files.photo.type
+      //console.log("product.photo.contentType: ", product.photo.contentType);
+
+    } // END if(files.photo)
+
+    // save data to new product schema,
+    // if all conditions are met. 
+    product.save((error, result) => {
+      if (error) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        })
+      } // END if(error)
+      res.json(result)
+
+    }) // END product.save()
+
+  }) // END form.parse
+
+}
+
+module.exports = { create, productById, read, removeProduct, updateProduct }; 
