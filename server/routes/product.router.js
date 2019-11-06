98857963b48d507d82aa64538357742c9884d68c
kaollie74 <kaollie74@gmail.com>
2019-11-06 16:09:26 -0600

    created productListRelated method to retrieve all products that exist in a single category

--- server/routes/product.router.js
+++ server/routes/product.router.js
@@ -4,7 +4,16 @@ const router = express.Router();
 
 
 // IMPORTING CONTROLLERS
-const { create, productById, productRead, removeProduct, updateProduct, productList } = require('../controllers/product.controller');
+const { 
+  create, 
+  productById, 
+  productRead, 
+  removeProduct, 
+  updateProduct, 
+  productList, 
+  productListRelated 
+} = require('../controllers/product.controller');
+
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth.controller');
 const { userById } = require('../controllers/user.controller');
 
@@ -13,6 +22,7 @@ const { userById } = require('../controllers/user.controller');
 // USER ROUTES
 router.get('/product/:productId', productRead)
 router.get('/products', productList)
+router.get('/products/related/:productId', productListRelated)
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, removeProduct );
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, updateProduct);
