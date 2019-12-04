f01563222e78e798af0fe502f7f5c5ecdda9c44c
kaollie74 <kaollie74@gmail.com>
2019-12-04 13:32:08 -0600

    created to functions to retrieve products by arrival and products by sold in Home.js

---
+++ src/core/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Cart = () => {
+  return (
+    <div className="col-4 mb-3">
+      <div className="card">
+        <div className="card-header">
+
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from './Layout';
+import {getProducts} from "./apiCore";
 
 const Home = () => {
+
+  // STATE
+  const [productBySell, setProductsBySell] = useState([])
+  const [productByArrival, setProductsByArrival] = useState([])
+  const [error, setError] = useState(false)
+
+  useEffect(()=> {
+    loadProductsByArrival()
+    loadProductsBySell();
+  } , [])
+
+  // function will invoke the getProducts() method that resides in /apiCore.
+  // it will pass in the argument sold so ti will be sorted by that agrument.
+  // if error set error to the error
+  // else set ProductBySell by the repsonse which will the the products sold that come from 
+  // the DB.
+  const loadProductsBySell = () => {
+
+    getProducts("sold")
+    .then(response => {
+      if(response.error) {
+        setError(response.error)
+      } else {
+        setProductsBySell(response)
+      }
+    })
+  }
+  const loadProductsByArrival = () => {
+
+    getProducts("createdAt")
+    .then(response => {
+      if(response.error) {
+        setError(response.error)
+      } else {
+        setProductsByArrival(response)
+      }
+    })
+  }
   
   return (
     <Layout title="Home Page" description="Node React E-commerce App">
-
+      { JSON.stringify(productByArrival)}
+      <br/>
+      <br/>
+      {JSON.stringify(productBySell)}
     </Layout>
   )
 }
